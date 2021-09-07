97268ce94ade514bb14832773883bc5758fba2bc
NightFeather <leolee50910@gmail.com>
2021-09-08 02:05:51 +0800

    make code cleaner

--- ytcf_app.py
+++ ytcf_app.py
@@ -8,8 +8,17 @@ print("----------------------------------------")
 print("")
 
 live_id = input("直播ID (網址結尾處編號): ")
-t = 0
-chat = pytchat.create(video_id=f"{live_id}", seektime = 0)
+retry = 0
+user = None
+chat = pytchat.create(video_id=f"{live_id}")
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(f" 聊天室已關閉 / 離線，將在{timer}後重試", end="\r")
+        time.sleep(1)
+        t -= 1
 
 def start_print(live_id):
     print("")
@@ -28,79 +37,79 @@ def end_print():
     print("")
     print("----------------------------------------")
 
+def print_chat(c):
+    print(f"{c.datetime} [{c.author.name}] - {c.message}")
+
+def txt_logging(c):
+    f = open('log.txt', 'a', encoding='utf-8')
+    f.write(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}\n")
+
+def main_run(mod_msg, txt_log, user_msg, user = None):
+    global retry
+    retry = 0
+    if txt_log == True:
+        open('log.txt', 'w', encoding='utf-8')
+    if user_msg == True:
+        start_print(live_id)
+        while chat.is_alive():
+            for c in chat.get().sync_items():
+                if c.author.name == f"{user}":
+                    if txt_log == True:
+                        txt_logging(c)
+                    print_chat(c)
+    else:
+        if mod_msg ==  True:
+            start_print(live_id)
+            while chat.is_alive():
+                for c in chat.get().sync_items():
+                    if c.author.isChatModerator == True:
+                        if txt_log == True:
+                            txt_logging(c)
+                        print_chat(c)
+        else:
+            start_print(live_id)
+            while chat.is_alive():
+                for c in chat.get().sync_items():
+                        if txt_log == True:
+                            txt_logging(c)
+                        print_chat(c)
+
+
 while True:
     user_msg = input("是否僅記錄特定成員訊息 (Y/N): ")
-    if user_msg == "n" or user_msg == "N":
-        while True:
-            mod_msg = input("是否僅記錄管理員訊息 (Y/N): ")
-            if mod_msg == "n" or mod_msg == "N":
-                while True:
-                    txt_log = input("是否輸出聊天紀錄 (Y/N): ")
-                    if txt_log == "n" or txt_log == "N":
-                        start_print(live_id)
-                        while chat.is_alive():
-                            for c in chat.get().sync_items():
-                                print(f"{c.datetime} [{c.author.name}] - {c.message}")
-                        end_print()
-                    if txt_log == "y" or txt_log == "Y":
-                        start_print(live_id)
-                        f = open('log.txt', 'w', encoding='utf-8')
-                        while chat.is_alive():
-                            for c in chat.get().sync_items():
-                                f = open('log.txt', 'a', encoding='utf-8')
-                                f.write(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}\n")
-                                print(f"{c.datetime} [{c.author.name}] - {c.message}")
-                        end_print()
-            if mod_msg == "y" or mod_msg == "Y":
-                while True:
-                    txt_log = input("是否輸出聊天紀錄 (Y/N): ") 
-                    if txt_log == "n" or txt_log == "N":
-                        start_print(live_id)
-                        while chat.is_alive():
-                            for c in chat.get().sync_items():
-                                if c.author.isChatModerator == True:
-                                    print(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}")
-                        end_print()
-                    if txt_log == "y" or txt_log == "Y":
-                        start_print(live_id)
-                        f = open('log.txt', 'w', encoding='utf-8')
-                        while chat.is_alive():
-                            for c in chat.get().sync_items():
-                                if c.author.isChatModerator == True:
-                                    f = open('log.txt', 'a', encoding='utf-8')
-                                    f.write(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}\n")
-                                    print(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}")
-                        end_print()
+    if user_msg.lower() == "n":
+        user_msg = False
+        mod_msg = input("是否僅記錄管理員訊息 (Y/N): ")
+        if mod_msg.lower() == "n":
+            mod_msg = False
+            txt_log = input("是否輸出聊天紀錄 (Y/N): ")
+            if txt_log.lower() == "n":
+                txt_log = False
+                break
+            elif txt_log.lower() == "y":
+                txt_log = True
+                break
             else:
                 continue
-            
-    if user_msg == "y" or user_msg == "Y":
+        elif mod_msg.lower() == "y":
+            mod_msg = True
+        else:
+            continue
+    elif user_msg.lower() == "y":
         user_msg = True
-        while True:
-            user = input("成員名稱: ") or None
-            if user != None:
-                while True:
-                    txt_log = input("是否輸出聊天紀錄 (Y/N): ")
-                    if txt_log == "n" or txt_log == "N":
-                        start_print(live_id)
-                        while chat.is_alive():
-                            for c in chat.get().sync_items():
-                                if c.author.name == f"{user}":
-                                    print(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}")
-                        end_print()
-                    if txt_log == "y" or txt_log == "Y":
-                        start_print(live_id)
-                        f = open('log.txt', 'w', encoding='utf-8')
-                        while chat.is_alive():
-                            for c in chat.get().sync_items():
-                                if c.author.name == f"{user}":
-                                    f = open('log.txt', 'a', encoding='utf-8')
-                                    f.write(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}\n")
-                                    print(f"{c.datetime} [{c.author.name}] - {c.message} {c.amountString}")
-                        end_print()
-                    else:
-                        continue
-            else:
-                continue
+        user = input("成員名稱: ") or None
+        break
     else:
         continue
+
+while True:
+    main_run(mod_msg, txt_log, user_msg, user)
+    if chat.is_alive() == False:
+        print("")
+        print("----------------------------------------")
+        print("")
+        countdown(150)
+        chat = pytchat.create(video_id=f"{live_id}")
+        retry += 1
+        if retry >= 5:
+            break
